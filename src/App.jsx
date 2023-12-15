89b7fcd91aa53929f4cdedfabcd7212e69b6e220
Angela <antunezangela89@gmail.com>
2023-12-15 23:03:52 +0100

    Clean up template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Router from './configuration/Router'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Router></Router>
