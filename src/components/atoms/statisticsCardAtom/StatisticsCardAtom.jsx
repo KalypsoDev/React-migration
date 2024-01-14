d86898cd04a29d9eef3902c7bab44246b3812ff3
Angela <antunezangela89@gmail.com>
2024-01-14 16:43:30 +0100

    Create StatisticsCardAtom component

---
+++ src/components/atoms/statisticsCardAtom/StatisticsCardAtom.jsx
@@ -0,0 +1,13 @@
+import "./statisticsCardAtom.module.css"
+
+export function StatisticsCardAtom({ idIcon, icon, value, label }) {
+  return (
+    <div className="card--colored card--grey">
+      <div id={idIcon}>
+        <i className={icon}></i>
+      </div>
+      <h2>{value}</h2>
+      <p>{label}</p>
+    </div>
+  );
+}
