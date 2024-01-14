22183da4e65a456c5b6e5a41bc8ca372cde1d682
Angela <antunezangela89@gmail.com>
2024-01-14 18:13:51 +0100

    Create HomeCardAtom component

---
+++ src/components/atoms/homeCardAtom/HomeCardAtom.jsx
@@ -0,0 +1,12 @@
+import "./homeCardAtom.module.css"
+
+export function HomeCardAtom({ idIcon, icon, titleIcon }) {
+  return (
+    <div className="card--colored">
+        <div className="card--padding" id={`${idIcon}--background`}>
+            <i className={icon}/>
+        </div>
+        <h4>{titleIcon}</h4>
+    </div>
+  );
+}
