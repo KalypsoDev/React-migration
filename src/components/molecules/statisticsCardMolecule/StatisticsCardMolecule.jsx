035a22675da660674554c115c8afab66ad0e4413
Angela <antunezangela89@gmail.com>
2024-01-14 16:44:08 +0100

    Create StatisticsCardMolecule component

---
+++ src/components/molecules/statisticsCardMolecule/StatisticsCardMolecule.jsx
@@ -0,0 +1,19 @@
+import { StatisticsCardAtom } from "../../atoms/statisticsCardAtom/StatisticsCardAtom";
+import statistics from "../../../contents/statistics.json";
+import "./statisticsCardMolecule.module.css";
+
+export function StatisticsCardMolecule() {
+  return (
+    <div id="statistics__cards">
+      {statistics.map(({ idIcon, icon, value, label }) => (
+        <StatisticsCardAtom
+          idIcon={idIcon}
+          icon={icon}
+          value={value}
+          label={label}
+          key={icon}
+        />
+      ))}
+    </div>
+  );
+}
