43e863ef8c288279f32752298560d48eef7f0803
Angela <antunezangela89@gmail.com>
2024-01-16 20:57:14 +0100

    Some examples of module css and how to use them

--- src/components/atoms/homeCardAtom/HomeCardAtom.jsx
+++ src/components/atoms/homeCardAtom/HomeCardAtom.jsx
@@ -1,9 +1,14 @@
 import "./homeCardAtom.module.css"
+import cardColoredStyles from '../../../styles/cardColored.module.css'
+//Estilos compartidos por varios componentes
+import styles from './homeCardAtom.module.css'
+//Estilos propios del componente
 
 export function HomeCardAtom({ idIcon, icon, titleIcon }) {
   return (
-    <div className="card--colored">
-        <div className="card--padding" id={`${idIcon}--background`}>
+    <div className={cardColoredStyles['card--colored']}>
+      {/*Se pone el nombre del archivo que he importado y luego, normalmente con puntos (si hay guiones se usan corchetes), se añade la clase*/}
+        <div className={styles['card--padding']} id={`${idIcon}--background`}>
             <i className={icon}/>
         </div>
         <h4>{titleIcon}</h4>
--- src/components/atoms/statisticsCardAtom/StatisticsCardAtom.jsx
+++ src/components/atoms/statisticsCardAtom/StatisticsCardAtom.jsx
@@ -1,8 +1,9 @@
 import "./statisticsCardAtom.module.css"
+import cardColoredStyles from '../../../styles/cardColored.module.css'
 
 export function StatisticsCardAtom({ idIcon, icon, value, label }) {
   return (
-    <div className="card--colored card--grey">
+    <div className={`${cardColoredStyles['card--colored']} card--grey`}>
       <div id={idIcon}>
         <i className={icon}/>
       </div>
