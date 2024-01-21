8b52519baff52c94fb75973fe0efeb958cb949b8
Angela <antunezangela89@gmail.com>
2024-01-21 12:50:59 +0100

    Create FooterFeaturesCardAtom component

---
+++ src/components/atoms/cards/footerFeaturesCardAtom/FooterFeaturesCardAtom.jsx
@@ -0,0 +1,21 @@
+import "./footerFeaturesCardAtom.css"
+
+export function FooterFeaturesCardAtom({ title, items }) {
+  return (
+    <div className="links__cards">
+        <h3>{title}</h3>
+        <ul>
+            {
+                items.map(({ icon, href, text }) => (
+                    <li key={text}>
+                        {
+                            icon && <a href={href}><i className={icon}/></a>
+                        }
+                        {text}
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
+  )
+}
