c5b1109540c771fba6a7eab8467446596afb93f7
Angela <antunezangela89@gmail.com>
2024-01-24 15:35:39 +0100

    Create TestView component for the class review of React

---
+++ src/components/views/testView/TestView.jsx
@@ -0,0 +1,9 @@
+export function TestView(){
+    //AQUÍ SE ESCRIBE LA LÓGICA
+    return(
+        <>
+            <h1>Holi</h1>
+            <p>¿Cómo están ustedes?</p>
+        </>
+    )
+}
--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomeView } from "../components/views/homeView/HomeView";
+import { TestView } from "../components/views/testView/TestView"
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeView />}></Route>
+        <Route path="/test-view" element={<TestView />}></Route>
       </Routes>
     </BrowserRouter>
   );
